559ec43cef2c8f2aa277e00761f115fb12cc2ee1
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-06-22 19:00:22 +0100

    Update project

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function myMode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+let body = document.body;
+let toggleBtn = document.querySelector(".toggle-btn");
+let currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+  body.classList.add("dark-theme");
 }
+toggleBtn.addEventListener("click", function () {
+  body.classList.toggle("dark-theme");
+
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem("currentTheme", "themeActive");
+  } else {
+    localStorage.removeItem("currentTheme");
+  }
+});
